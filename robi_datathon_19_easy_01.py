eab7e3df15e4ad88424ba308335b8f130f081cb5
Rubaiyat Islam Rafat <rafat.mit.806@gmail.com>
2019-04-23 23:13:31 +0600

    Update robi_datathon_19_easy_01.py

--- robi_datathon_19_easy_01.py
+++ robi_datathon_19_easy_01.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+
+__author__ = "Rubaiyat Islam Rafat"
+__copyright__ = "Copyright 2019, Eagle Eye Lab"
+__credits__ = ["Rubaiyat Islam Rafat"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Rubaiyat Islam Rafat"
+__email__ = "rob@spot.colorado.edu"
+__status__ = "Production"
+
 import os
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
 os.environ["SPARK_HOME"] = "/content/spark-2.3.3-bin-hadoop2.7"
